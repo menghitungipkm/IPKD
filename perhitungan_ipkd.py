11f23d2fbdb080dbdc4172845d5e9f25b74accfb
menghitungipkm <menghitungipkm@gmail.com>
2024-10-01 10:36:23 +0700

    Update perhitungan_ipkd.py

--- perhitungan_ipkd.py
+++ perhitungan_ipkd.py
@@ -34,10 +34,10 @@ def bagi_data_per_kota_kabupaten_dan_tahun(df):
     max_values = df.select_dtypes(include='number').max()
 
     bobot_data = [
-        4, 5, 1, 4, 5, 4, 4, 5, 1, 3,
-        2, 1, 1, 5, 3, 5, 1, 2, 2, 4,
-        2, 4, 5, 5, 5, 5, 3, 2, 5, 4,
-        5, 2, 3, 5, 3, 1, 5, 5, 3
+        5, 5, 5, 4, 2, 3, 5, 5, 4, 5,
+        5, 3, 5, 2, 2, 3, 2, 1, 3, 1,
+        5, 4, 5, 4, 1, 5, 5, 2, 2, 3,
+        5, 3, 1, 1, 4, 5, 4, 4, 
     ]
 
     kategori = [
